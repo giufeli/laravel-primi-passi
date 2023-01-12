bf86da05f3019aa21ab9101c150e0a13b2896f41
giufeli <giuliofelicetti99@gmail.com>
2023-01-12 16:22:16 +0100

    Commit

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,10 +1,21 @@
 <!DOCTYPE html>
+<?php
+
+$info = "Laravel è un framework open source di tipo MVC scritto in PHP per lo sviluppo di applicazioni web, creato nel 2011 da Taylor Otwell come derivazione di Symfony. "
+
+$array = array(
+    "sito" => "https://laravel.com/",
+    "foto" => "https://www.google.com/search?q=laravel&rlz=1C1ONGR_itIT993IT993&source=lnms&tbm=isch&sa=X&ved=2ahUKEwis4Zu7psL8AhVhSfEDHRoGAx4Q_AUoAnoECAEQBA&biw=1920&bih=937&dpr=1",,
+);
+
+?>
+
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>Laravel Home</title>
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@200;600&display=swap" rel="stylesheet">
@@ -15,7 +26,8 @@
                 background-color: #fff;
                 color: #636b6f;
                 font-family: 'Nunito', sans-serif;
-                font-weight: 200;
+                font-weight: 300;
+                font-size: 20px;
                 height: 100vh;
                 margin: 0;
             }
@@ -65,35 +77,17 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Hello World questo è Laravel !
                 </div>
-
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
-                </div>
+                <div>{{ $info }}</div>
+                <div>Ma cos'è Laravel?</div>
+                <ul>
+                    @foreach ($fruits as $item)
+                        <li>{{ $item }}</li>
+                    @endforeach
+                </ul>
             </div>
         </div>
     </body>
